580c1d82922eb8c6cde38a30ba6f7baa4c67374b
Lucía Enreda <lucia.cano@enreda.coop>
2023-10-17 10:56:54 +0200

    Add loggedNavigation and Auth at APP

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+import {LoggedNavigation} from "./routes";
+import {Auth} from "./pages";
+import "./App.scss";
+function App() {
+ const [user, setUser] = useState(undefined);
+ const auth = getAuth();
+
+  onAuthStateChanged(auth, (user)=> {
+    setUser(user);
+  });
+  
+ if (user === undefined) return null;
+
+  return user ? <LoggedNavigation/> : <Auth/>
+}
+
+export default App;
