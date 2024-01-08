c69233a9d2a567a6cf3dd260d3ae30c8f7918786
Lucía Enreda <lucia.cano@enreda.coop>
2024-01-08 18:27:58 +0100

    adding formik

--- src/components/Auth/RegisterForm/RegisterForm.jsx
+++ src/components/Auth/RegisterForm/RegisterForm.jsx
@@ -1,74 +1,68 @@
 import React from "react";
 import { useState } from "react";
 import { Box, Button, Grid, TextField } from "@mui/material";
+import { useFormik } from "formik";
+import { initialValues } from "./RegisterForm.data";
 import "./registerForm.scss";
 
 export function RegisterForm({ goBack }) {
-  const [emailValue, setEmailValue] = useState("");
-  const [errors, setErrors] = useState({
-    email: false,
-    // Otros campos que puedan tener errores
+  const formik = useFormik({
+    initialValues: initialValues(),
+    onSubmit: (formValue) => {
+      console.log("REgister OK");
+      console.log(formValue);
+    },
   });
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // Validar el formato del correo electrónico
-    const isEmailValid = validateEmail(emailValue);
-
-    setErrors({
-      email: !isEmailValid,
-      // Otros campos que puedan tener errores
-    });
-
-    if (isEmailValid) {
-      // Realizar acciones adicionales si el formulario es válido
-    }
-  };
-
-  const validateEmail = (email) => {
-    // Expresión regular para verificar el formato del correo electrónico
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
 
   return (
     <Box className="register-container">
       <h1>Crea tu cuenta gratis</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <TextField
-              error={errors.email}
-              onChange={(e) => setEmailValue(e.target.value)}
+              // error={errors.email}
+              // onChange={(e) => setEmailValue(e.target.value)}
               required
+              name="email"
+              value={formik.values.email}
               label="Email"
               fullWidth
               id="outlined-size-small"
-              helperText={errors.email ? "Incorrect entry" : ""}
+              onChange={formik.handleChange}
+              // helperText={errors.email ? "Incorrect entry" : ""}
             />
           </Grid>
           <Grid item xs={6}>
             <TextField
               required
+              name="username"
+              value={formik.values.username}
               label="Name"
               id="outlined-size-small"
+              onChange={formik.handleChange}
               fullWidth
             />
           </Grid>
           <Grid item xs={6}>
             <TextField
               required
+              name="lastname"
+              value={formik.values.lastname}
               label="Last Name"
               id="outlined-size-small"
+              onChange={formik.handleChange}
               fullWidth
             />
           </Grid>
           <Grid item xs={12}>
             <TextField
               required
+              name="password"
+              value={formik.values.password}
               label="Password"
               type="password"
+              onChange={formik.handleChange}
               fullWidth
               id="outlined-size-small"
             />
