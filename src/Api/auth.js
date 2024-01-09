e8d47d630858b390aaffc8eea17f82f87cdf40da
Lucía Enreda <lucia.cano@enreda.coop>
2024-01-09 10:44:56 +0100

    Register class for user Login

---
+++ src/Api/auth.js
@@ -0,0 +1,24 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+export class Auth {
+  async register(email, password) {
+    try {
+      const auth = getAuth();
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async logout() {
+    try {
+      const auth = getAuth();
+      await signOut(auth);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
