4a6f99bcfd0ba9445c89d95733769420c90a0908
Lucía Enreda <lucia.cano@enreda.coop>
2024-01-22 20:19:13 +0100

    Add TopBar structure and funtionalities

---
+++ src/components/Layouts/TopBar/TopBar.jsx
@@ -0,0 +1,39 @@
+import { Box, Avatar } from "@mui/material";
+import { Auth, User } from "../../../Api";
+import { ArrowBackIos, ExitToApp } from "@material-ui/icons";
+import { Link, useNavigate } from "react-router-dom";
+import "./topBar.scss";
+
+const auth = new Auth();
+const user = new User();
+
+export function TopBar() {
+  const navigation = useNavigate();
+  const userData = user.getMe();
+
+  console.log(userData);
+
+  const displayName = userData.displayName || "Mi cuenta";
+  const avatar = userData.photoURL;
+
+  const goBack = () => {
+    navigation(-1);
+  };
+
+  return (
+    <Box className="top-bar">
+      <ArrowBackIos onClick={goBack} className="top-bar__goBack" />
+      <Box className="top-bar__endBar">
+        <h3>¡Hello {displayName}!</h3>
+        <Link to="/profile" className="top-bar__profile">
+          <Avatar src={avatar} alt={displayName}></Avatar>
+        </Link>
+        <ExitToApp
+          fontSize="large"
+          className="top-bar__exit"
+          onClick={auth.logout}
+        />
+      </Box>
+    </Box>
+  );
+}
