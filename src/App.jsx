0762fe4a2017ef9ca11b72f0f1bc20c4477073b8
Lucía Enreda <lucia.cano@enreda.coop>
2024-01-22 20:15:41 +0100

    Adding logged validations

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
-import {getAuth, onAuthStateChanged} from "firebase/auth";
-import {LoggedNavigation} from "./routes";
-import {Auth} from "./pages";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { LoggedNavigation } from "./routes";
+import { Auth } from "./Pages";
 import "./App.scss";
 function App() {
- const [user, setUser] = useState(undefined);
- const auth = getAuth();
+  const [user, setUser] = useState(undefined);
+  const auth = getAuth();
 
-  onAuthStateChanged(auth, (user)=> {
+  onAuthStateChanged(auth, (user) => {
     setUser(user);
   });
-  
- if (user === undefined) return null;
 
-  return user ? <LoggedNavigation/> : <Auth/>
+  if (user === undefined) return null;
+
+  return user ? <LoggedNavigation /> : <Auth />;
 }
 
 export default App;
