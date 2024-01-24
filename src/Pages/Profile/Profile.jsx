1b3aa9acd7308968414fd4562cb9189a6758f840
Lucía Enreda <lucia.cano@enreda.coop>
2024-01-24 10:55:19 +0100

    Create profile component

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,8 +1,45 @@
+import { Box, Button } from "@mui/material";
+import { User } from "../../Api";
+import "./profile.scss";
+import { UpdateAvatar } from "../../components/Profile";
 
-export function Profile(){
-    return(
-        <div>
-            <h1>Profile</h1>
-        </div>
-    )
+const userController = new User();
+
+export function Profile() {
+  const { displayName, email } = userController.getMe();
+  return (
+    <Box className="profile">
+      <h1>Profile Config</h1>
+      <Box className="profile__block">
+        <Box className="">
+          <UpdateAvatar />
+          <p> Username: {displayName || "User Name"}</p>
+        </Box>
+        <Button
+          variant="contained"
+          onClick={() => console.log("cambiar displeyName")}
+        >
+          Update
+        </Button>
+      </Box>
+      <Box className="profile__block">
+        <p>Email: {email}</p>
+        <Button
+          variant="contained"
+          onClick={() => console.log("cambiar email")}
+        >
+          Update
+        </Button>
+      </Box>
+      <Box className="profile__block">
+        <p>Contraseña: ******** </p>
+        <Button
+          variant="contained"
+          onClick={() => console.log("cambiar conttraseña")}
+        >
+          Update
+        </Button>
+      </Box>
+    </Box>
+  );
 }
