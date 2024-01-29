fa56a1e01c59e06d323d0ed9dc07361cda0c6b23
Lucía Enreda <lucia.cano@enreda.coop>
2024-01-29 20:26:08 +0100

    Save at  avatar files at firebase

--- src/components/Profile/UpdateAvatar/UpdateAvatar.jsx
+++ src/components/Profile/UpdateAvatar/UpdateAvatar.jsx
@@ -2,18 +2,21 @@ import { Box, Avatar } from "@mui/material";
 import { defaultUser } from "../../../assets";
 import { useDropzone } from "react-dropzone";
 import { useCallback, useState } from "react";
-import { User } from "../../../Api";
+import { User, Storage } from "../../../Api";
 import "./updateAvatar.scss";
 
 const user = new User();
+const storageController = new Storage();
 
 export function UpdateAvatar() {
-  const { photoURL } = user.getMe();
+  const { photoURL, uid } = user.getMe();
   const [avatarUrl, setAvatarUrl] = useState(photoURL || defaultUser);
 
   const onDrop = useCallback(async (acceptedFile) => {
     const file = acceptedFile[0];
     setAvatarUrl(URL.createObjectURL(file));
+    const response = await storageController.uploadFile(file, "avatar", uid);
+    console.log(response);
   });
 
   const { getInputProps, getRootProps } = useDropzone({ onDrop });
