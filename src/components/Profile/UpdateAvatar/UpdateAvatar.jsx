f1704e8e7dfa0f1321073b58f888079d4fff5880
Lucía Enreda <lucia.cano@enreda.coop>
2024-01-29 18:41:15 +0100

    Add drag and drop at Avatar

--- src/components/Profile/UpdateAvatar/UpdateAvatar.jsx
+++ src/components/Profile/UpdateAvatar/UpdateAvatar.jsx
@@ -1,10 +1,27 @@
 import { Box, Avatar } from "@mui/material";
 import { defaultUser } from "../../../assets";
+import { useDropzone } from "react-dropzone";
+import { useCallback, useState } from "react";
+import { User } from "../../../Api";
 import "./updateAvatar.scss";
+
+const user = new User();
+
 export function UpdateAvatar() {
+  const { photoURL } = user.getMe();
+  const [avatarUrl, setAvatarUrl] = useState(photoURL || defaultUser);
+
+  const onDrop = useCallback(async (acceptedFile) => {
+    const file = acceptedFile[0];
+    setAvatarUrl(URL.createObjectURL(file));
+  });
+
+  const { getInputProps, getRootProps } = useDropzone({ onDrop });
+
   return (
-    <Box className="avatar">
-      <Avatar className="avatar__img" src={defaultUser} alt={defaultUser} />
+    <Box className="avatar" {...getRootProps()}>
+      <input type="file" {...getInputProps()} />
+      <Avatar className="avatar__img" src={avatarUrl} />
     </Box>
   );
 }
